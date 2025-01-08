0d0825bea6a1310b693deac5552b7335039f6b04
UlysselaGlisse <140060856+UlysselaGlisse@users.noreply.github.com>
2025-01-08 13:39:40 +0100

    mise-à-jour info

--- ui/main_dialog/info_dialog.py
+++ ui/main_dialog/info_dialog.py
@@ -16,10 +16,10 @@ class InfoDialog(QDialog):
         Ce plugin apporte une assistance dans la réalisation de compositions de segments.<br><br>
         <i>Instructions :</i><br>
         <ol>
-            <li><b>Sélectionnez les couches :</b><br />
+            <b>Sélectionnez les couches :</b><br />
                 - Une couche segments avec un champ 'id' qui sera utilisé pour faire vos compositions.<br />
-                - Une couche compositions avec un champ dans lequel vous entrez les listes de segments (sans espaces et séparées par une virgule, par exemple 1,2,3).</li>
-            <li><b>Panier à segments :</b><br />
+                - Une couche compositions avec un champ dans lequel vous entrez les listes de segments (sans espaces et séparées par une virgule, par exemple 1,2,3).<br /><br />
+            <b>Panier à segments :</b><br />
                 Cliquez sur l'icône, une petite bulle apparaîtra à droite du curseur. Sélectionnez les segments qui vous intéressent. La liste se remplira.<br />
                 L'outil cherchera toujours à combler les trous entre deux segments.<br />
                 Vous pouvez appuyer sur <b>z</b> pour retirer le dernier segment ajouté à la liste et sur <b>e</b> pour le rétablir.</li>
